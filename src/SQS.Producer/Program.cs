d644a71043ff4681371a69a335c3bcc702dd66e5
EricHosokawa <erichosokawa@gmail.com>
2021-06-26 14:22:48 -0300

    commit all solution

---
+++ src/SQS.Producer/Program.cs
@@ -0,0 +1,44 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace SQS.Producer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("My AWS SQS Producer!");
+
+            try
+            {
+                var client = new AmazonSQSClient(RegionEndpoint.USEast2);
+
+                for(int i = 0; i < 4; i++)
+                {
+                    var message = "teste" + 1;
+
+                    var request = new SendMessageRequest
+                    {
+                        QueueUrl = "https://sqs.us-east-2.amazonaws.com/713434528261/teste_v1",
+                        MessageBody = message
+                    };
+
+                    await client.SendMessageAsync(request);
+
+                    Console.WriteLine($"Send SQS MessageBody sucessfully. Message: {message}");
+
+                    await Task.Delay(new TimeSpan(0, 0, 10));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + ex.StackTrace);
+
+                throw ex;
+            }
+        }
+    }       
+}
